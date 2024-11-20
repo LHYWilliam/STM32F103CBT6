d1943bdee55c56685f2dff42fdeef92c44be91bf
LHYLiuWilliam <LHYLiuWilliam@gmail.com>
2024-11-20 22:48:23 +0800

    Use MQx_DataLength

--- Source/interrupt.c
+++ Source/interrupt.c
@@ -3,6 +3,8 @@
 
 #include "Sampler.h"
 
+#define MQx_DataLength 128
+
 extern Sampler_t Sampler;
 
 extern int16_t MQ3_Index;
@@ -13,10 +15,10 @@ extern uint16_t MQ135_Data[128];
 
 void TIM3_IRQHandler() {
     if (TIM_GetITStatus(Sampler.TIMx, TIM_IT_Update)) {
-        MQ3_Index = (MQ3_Index + 1) % 128;
+        MQ3_Index = (MQ3_Index + 1) % MQx_DataLength;
         MQ3_Data[MQ3_Index] = Sampler.Data[0];
 
-        MQ135_Index = (MQ135_Index + 1) % 128;
+        MQ135_Index = (MQ135_Index + 1) % MQx_DataLength;
         MQ135_Data[MQ135_Index] = Sampler.Data[1];
 
         TIM_ClearITPendingBit(Sampler.TIMx, TIM_IT_Update);
